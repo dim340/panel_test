86b7d359993b4019eadc9a79b57f1b56b5576f6e
dim340 <64194622+dim340@users.noreply.github.com>
2020-04-23 14:45:06 +0200

    Update serverlaunch.py

--- serverlaunch.py
+++ serverlaunch.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["panel", "serve", "panelapp"])
+    Popen(["panel", "serve", "panelapp/class_launch_server.py"])
